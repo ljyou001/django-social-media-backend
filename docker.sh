a416d58f8a57702b1efc20a305cc07130e5a92c7
Oliver <ljyou001@live.com>
2023-06-03 11:24:26 +0800

    setup with files

---
+++ docker.sh
@@ -0,0 +1,25 @@
+# (EXT) docker run -p 80:8000 -p 9005:22 -v ".:/vagrant" -i -t --name mineos ubuntu:18.04 bash
+
+apt install vim python3 python3-pip wget dos2unix sudo lsb-release iproute2
+
+passwd root
+
+adduser ljyou001
+
+usermod -aG sudo ljyou001
+
+echo 'export PATH="/home/ljyou001/.local/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+
+cd /vagrant
+dos2unix provision.sh
+dos2unix requirements.txt
+./provision.sh
+
+sudo apt install openssh-server
+
+sudo vim /etc/ssh/sshd_config
+# 使⽤vim打开并修改配置⽂件，找到PermitRootLogin prohibit-password 这⼀⾏，修改为PermitRootLogin
+# yes ，允许通过ssh远程访问docker
+
+sudo service ssh restart
