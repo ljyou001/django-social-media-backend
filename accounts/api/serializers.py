2453d516f37e5fc6ca3fffed88114de9115e6329
Oliver <ljyou001@live.com>
2023-06-03 14:50:59 +0800

    login

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -5,3 +5,8 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'email')
+
+class LoginSerializer(serializers.Serializer):
+    # 仅用来帮助检测是否有这两项，CharField里面required默认为True
+    username = serializers.CharField()
+    password = serializers.CharField()
--- accounts/api/views.py
+++ accounts/api/views.py
@@ -3,8 +3,12 @@ from rest_framework import viewsets
 from rest_framework import permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from accounts.api.serializers import UserSerializer
-from django.contrib.auth import logout as django_logout
+from accounts.api.serializers import UserSerializer, LoginSerializer
+from django.contrib.auth import (
+    logout as django_logout,
+    login as django_login,
+    authenticate as django_authenticate
+)
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
@@ -22,7 +26,10 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
 # permission_classes：针对是否登录之后进行检测
 
 class AccountViewSet(viewsets.ViewSet):
-    
+    # 这里所用的viewset是一个空的viewset，全部自己定义想要的东西
+    serializer_class = LoginSerializer
+    # You can define a default serializer for this class
+
     @action(methods=['get'], detail=False)
     # 自定义了一个动作来操作
     # detail=False：意味着这是定义在整体资源上的一个动作，不需要写objectID
@@ -36,5 +43,41 @@ class AccountViewSet(viewsets.ViewSet):
     def logout(self, request):
         django_logout(request)
         return Response({'success': True})
+    
+    @action(methods=['post'], detail=False)
+    def login(self, request):
+        # get username and passwrod from request
+        # normally from request.data['username'], but this one could be void
+        serializer = LoginSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response({
+                "success": False,
+                "manage": "Please check your input",
+                "errors": serializer.errors
+            }, status=400)
+        username = serializer.validated_data['username']
+        password = serializer.validated_data['password']
+        # You can obtain data directly from serializer, and it will help you with some essential transformations
 
-# 这里所用的viewset是一个空的viewset，全部自己定义想要的东西
+        # If you would like to know how the sql query executed in the BG:
+        # queryset = User.objects.filter(username=username)
+        # print(queryset.query)
+        if not User.objects.filter(username=username).exists():
+            return Response({
+                "success": False,
+                "manage": "User does not exist"
+            }, status=400)
+        # Normally we will not user this though
+
+        user = django_authenticate(username=username, password=password)
+        # django_login only accept the user gone through the django_authenticate
+        if not user or user.is_anonymous:
+            return Response({
+                "success": False,
+                "manage": "Invalid username or password"
+            }, status=400)
+        django_login(request, user)
+        return Response({
+            "success": True,
+            "user": UserSerializer(instance=user).data,
+        }, status=200)
