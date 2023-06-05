90b935bd891fd667a1529dbccc6aef9460ddc2dd
Oliver <ljyou001@live.com>
2023-06-05 16:10:20 +0800

    tweets models

---
+++ tweets/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+# import datetime
+from utils.time_helper import utc_now
+# Create your models here.
+
+class Tweet(models.Model):
+    user = models.ForeignKey(
+        User, 
+        on_delete=models.SET_NULL, # In production, always use set_null for on delete
+        null=True, 
+        help_text='The user who posted this tweet',
+    )
+    # too long for one line? one line one parm, "," in the last line
+    content = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True) # auto_now_add only update while creating
+    updated_at = models.DateTimeField(auto_now=True) # auto_now: update every time
+
+    @property
+    def hours_to_now(self):
+        # return (datetime.datetime.now() - self.created_at).seconds / 3600
+        # TypeError: can't subtract offset-naive and offset-aware datetimes
+        # This is because now() have no time zone. but created_at has time zone. 
+        return (utc_now() - self.created_at).seconds / 3600
+    
+    def __str__(self):
+        # This function define the string representation while you print the instance
+        return f'{self.created_at} {self.user}: {self.content}'
---
+++ tweets/tests.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from tweets.models import Tweet
+from datetime import timedelta
+from utils.time_helper import utc_now
+# Create your tests here.
+
+class TweetTests(TestCase):
+    
+    def test_hours_to_now(self):
+        youge = User.objects.create(username='niyouge')
+        tweet = Tweet.objects.create(
+            user=youge,
+            content='hello world',
+        )
+        tweet.created_at = utc_now() - timedelta(hours=10)
+        tweet.save()
+        self.assertEqual(tweet.hours_to_now, 10)
+        # DO NOT use hours_to_now(), property should use no ()
