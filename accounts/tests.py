cd26aec7154925eae4263ddd582d514f49066225
Oliver <ljyou001@live.com>
2023-06-21 01:44:21 +0800

    UserProfile model+tests and django-storage for avatar on AWS/OCI

---
+++ accounts/tests.py
@@ -0,0 +1,11 @@
+from accounts.models import UserProfile
+from testing.testcases import TestCase
+
+
+class UserProfileTest(TestCase):
+    def test_profile_property(self):
+        user1 = self.create_user('user1')
+        self.assertEqual(UserProfile.objects.count(), 0)
+        profile = user1.profile
+        self.assertEqual(isinstance(profile, UserProfile), True)
+        self.assertEqual(UserProfile.objects.count(), 1)
