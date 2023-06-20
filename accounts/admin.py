e583d16f7c7af5de34cfdd1782e1be0e03a8b9b1
Oliver <ljyou001@live.com>
2023-06-21 01:47:20 +0800

    UserProfile model+tests and django-storage for avatar on AWS/OCI

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from accounts.models import UserProfile
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'nickname', 'avatar', 'created_at', 'updated_at')
+    date_hierarchy = 'created_at'
+
+
+class UserProfileInline(admin.StackedInline):
+    """
+    Why this class?
+    You can change the user profile content while you are modifying the User model
+    """
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'user_profiles'
+
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ('username', 'email', 'is_staff', 'date_joined')
+    date_hierarchy = 'date_joined'
+    inlines = (UserProfileInline, )
+
+
+admin.site.unregister(User)
+# This is to unregister the existing User model in the admin panel 
+admin.site.register(User, UserAdmin)
+# This is to register our own User model in the admin panel
