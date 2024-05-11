eb5f911026fbd947d4e5659d507298dd1a7535bb
Oliver Lyu <ljyou001@live.com>
2024-05-10 18:30:19 -0700

    utilize for new deploy

--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
 # (EXT) docker run -p 80:8000 -p 9005:22 -v ".:/vagrant" -i -t --name mineos ubuntu:18.04 bash
 
-apt install vim python3 python3-pip wget dos2unix sudo lsb-release iproute2
+apt install vim python3 python3-pip wget dos2unix sudo lsb-release iproute2 
 
 passwd root
 
@@ -16,10 +16,12 @@ dos2unix provision.sh
 dos2unix requirements.txt
 ./provision.sh
 
-sudo apt install openssh-server
+sudo apt install openssh-server memcached redis
 
 sudo vim /etc/ssh/sshd_config
 # 使⽤vim打开并修改配置⽂件，找到PermitRootLogin prohibit-password 这⼀⾏，修改为PermitRootLogin
 # yes ，允许通过ssh远程访问docker
 
 sudo service ssh restart
+sudo service redis-server start
+/etc/init.d/memcached restart
